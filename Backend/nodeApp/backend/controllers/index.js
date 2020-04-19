c97894d2ed631c2d0c5642e93732bb96f031e6ea
Rodrigo Vargas <rodrigo.vargas@kairosds.com>
2020-04-20 01:05:30 +0200

    tests pass

--- Backend/nodeApp/backend/controllers/index.js
+++ Backend/nodeApp/backend/controllers/index.js
@@ -2,11 +2,28 @@
 
 const Commerce = require('../models/commerce')
 
-module.exports = (router) => {
-  router.get('/', findSearch)
+function validateParams(req, res, next) {
+  if (!req.query.lat && !req.query.lng) {
+    return res.status(400).json({ error: 'Invalid search params' })
+  }
+
+  next()
 }
 
-function findSearch(req, res) {
-  // TODO implement
-  res.status(500).send()
+module.exports = (router) => {
+  router.get('/', validateParams, findSearch)
+}
+
+async function findSearch(req, res) {
+  const { lng, lat } = req.query
+
+  Commerce.find({ location: [lng, lat] })
+    .then(result => {
+      result.length
+        ? res.json(result)
+        : res.status(204).send()
+    })
+    .catch(() => {
+      res.status(500).send()
+    })
 }
